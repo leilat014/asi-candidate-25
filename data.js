2ae26f0f41aea02e149c1654a4f894366a54d498
leilat014 <145727009+leilat014@users.noreply.github.com>
2025-04-22 16:44:32 -0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,91 @@
+export const quiz = {
+  totalQuestions: 7,
+  questions: [
+    {
+      id: 1,
+      question:
+        "How should the ASI president include diversity, equity and inclusion efforts in their leadership?",
+      answers: {
+        A: "Use past experience in DEI leadership to help students feel seen, heard and valued",
+        B: "Protect and expand scholarships for all student groups",
+        C: "Recruit a diverse cabinet that represents the student body",
+        D: "Take action to address the needs of students through creating new services and resources",
+      },
+    },
+    {
+      id: 2,
+      question:
+        "How should the ASI president support the student body through major transitions like the Cal Maritime integration and the semester conversion?",
+      answers: {
+        A: "Create a student advisory board to build trust between ASI and the student body",
+        B: "Hold public town hall meetings with President Armstrong",
+        C: "Ensure student fees are used appropriately and allow students to give feedback",
+        D: "Prioritize current Cal Poly students through the transition",
+      },
+    },
+    {
+      id: 3,
+      question:
+        "How should the ASI president address St. Patrick's Day celebrations?",
+      answers: {
+        A: "Increase the number of on-campus events for students year-round",
+        B: "Continue to host on-campus St. Patrick's Day celebrations",
+        C: "Protect and expand on current St. Patrick’s Day events",
+        D: "Focus on improving the relationship between Cal Poly students and the San Luis Obispo community",
+      },
+    },
+    {
+      id: 4,
+      question:
+        "How should ASI enhance campus events to foster student engagement and community?",
+      answers: {
+        A: "Protect and support student organizations on campus",
+        B: "Work with campus leaders to ensure events reflect what students really want",
+        C: "Support student clubs and organizations in hosting more engaging events to foster community",
+        D: "Work with the administration to create policy and events that help students find community",
+      },
+    },
+
+    {
+      id: 5,
+      question:
+        "How should ASI improve transportation and accessibility on and off campus?",
+      answers: {
+        A: "Utilize external partnerships to bring in innovative solutions",
+        B: "Prioritize student voices in local city government discussions",
+        C: "Focus on student-led feedback to inform better campus decisions",
+        D: "Expand bus routes to grocery stores and improve commuter parking affordability",
+      },
+    },
+
+    {
+      id: 6,
+      question:
+        "How should the ASI President support student organizations and club funding?",
+      answers: {
+        A: "Advocate for external partnerships that can help sponsor student clubs",
+        B: "Reform funding policy to ensure transparency",
+        C: "Make student fees work more directly for clubs and ensure clubs have the resources to thrive",
+        D: "Increase club funding and expand access to on-campus spaces for club meetings and events",
+      },
+    },
+
+    {
+      id: 7,
+      question:
+        "How should ASI build trust and accountability with the student body?",
+      answers: {
+        A: "Use past leadership roles to personally connect with students and advocate on their behalf",
+        B: "Host regular listening sessions and follow-up on student feedback",
+        C: "Launch campaigns and systems for students to easily share concerns with ASI and get answers",
+        D: "Ensure all student concerns are addressed through visible actions and results",
+      },
+    },
+  ],
+  results: {
+    A: "Alexis Espinoza",
+    B: "Reagan Denny",
+    C: "Colin Shea",
+    D: "Marc Cabeliza",
+  },
+};
